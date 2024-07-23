db71d8c1ccc35ea11108c57e9593c66d48f55385
Sudha1145 <sudha.xworkz@gmail.com>
2024-07-23 21:05:11 +0530

    Old File

---
+++ ex/Instance.java
@@ -0,0 +1,92 @@
+//class is non primitive datatype
+//implicit constructor ------>if we do not declare anything in the body of the class
+//here access specifier,parameter and throws are optional
+//Constructor helps to make copy of the object using new keyword
+//allocate to the instance memory
+//invoke constructor to the main method
+//return memory location of copy of the class instance
+
+class Helicopter
+{
+	
+	Helicopter(){
+	System.out.println("Created Helicopter");
+	}
+}
+class Market
+{
+	
+	Market(){
+	System.out.println("Created Market");
+	}
+}
+class Mall
+{
+	
+	Mall(){
+	System.out.println("Created Mall");
+	}
+}
+class Ring
+{
+	
+	Ring(){
+	System.out.println("Created Ring");
+	}
+}
+class Auto
+{
+	
+	Auto(){
+	System.out.println("Created Auto");
+	}
+}
+class Auditorium
+{
+	
+	Auditorium(){
+	System.out.println("Created Auditorium");
+	}
+}
+class SpaceShip
+{
+	SpaceShip(){
+	System.out.println("created SpaceShip");
+	}
+}
+class Bullet
+{
+	Bullet(){
+	System.out.println("Created Bullet");
+	}
+}
+class Galaxy
+{
+	Galaxy(){
+	System.out.println("Created Galaxy");
+	}
+}
+class Spoon
+{
+	Spoon(){
+	System.out.println("Created Spoon");
+	}
+}
+class BlackHole
+{
+	BlackHole(){
+	System.out.println("Created BlackHole");
+	}
+}
+class Cake
+{
+	Cake(){
+	System.out.println("Created Cake");
+	}
+}
+class Wrapper
+{
+	Wrapper(){
+	System.out.println("Created Wrapper");
+	}
+}
---
+++ ex/InstanceRunner.java
@@ -0,0 +1,22 @@
+class InstanceRunner
+{
+	public static void main(String[] values)
+	{
+		System.out.println("Start main in InstanceRunner");
+		Helicopter helicopter= new Helicopter();
+		Market market = new Market();
+		Mall mall = new Mall();
+		Ring ring = new Ring();
+		Auto auto = new Auto();
+		Auditorium auditorium = new Auditorium();
+		SpaceShip spaceShip = new SpaceShip();
+		Bullet bullet = new Bullet();
+		Galaxy galaxy = new Galaxy();
+		Spoon spoon = new Spoon();
+		BlackHole blackHole = new BlackHole();
+		Cake cake = new Cake();
+		Wrapper wrapper = new Wrapper();
+		System.out.println("End main in InstanceRunner");
+	}
+}
+		
